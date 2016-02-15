737759bbc8d654ff4fe545fe8e40f5eb132e939d
Chris Carley <magicmaster21@hotmail.com>
2016-02-14 16:55:01 -0800

    Added custom message support
    Replaced the baked in message string with an input on the module panel
    and a few variables so that the caster can set their own message.

--- slap.js
+++ slap.js
@@ -10,14 +10,16 @@ var modSlap = (function(){
 	
 	var _myTab = apiAddTab("Slap");
 	var _slapSettings = {
-		accessLevel: "all"
-	}
+		accessLevel: "all",
+		message: `[botname] lurches forth and slaps some sense into [target]!`
+	};
+	
 	var _settingsFileName = "modSlapSettings.ini";
 
 	// To save your settings, this isn't intended to be called from outside
 	var _saveSettings = function () {
 		apiWriteFile( _settingsFileName, JSON.stringify( _slapSettings ) );
-	}
+	};
 
 	// load gambling settings, if not create them
 	var _settingsFile = apiOpenFile( _settingsFileName );
@@ -25,7 +27,7 @@ var modSlap = (function(){
 		_saveSettings();
 	} else {
 		_slapSettings = $.parseJSON( _settingsFile ); // load it
-	}
+	};
 	
 	//The slap tab
 	$(_myTab).html(`<div class"row-fluid">
@@ -60,6 +62,18 @@ var modSlap = (function(){
 							</div>
 						</form>
 					</li>
+					<li class="list-group-item">
+						<p><strong>Message:</strong></p>
+						<input id="modSlapMessageInput" type"text" size="80">
+						<div class="panel-body">
+							<p>You can use the following tags in your message:</p>
+							<ul>
+								<li>[botname] - The name of your bot.</li>
+								<li>[user] - The name of the viewer using the command.</li>
+								<li>[target] - The target's name (Entered by the user).</li>
+							</ul>
+						</div>
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -67,8 +81,19 @@ var modSlap = (function(){
 	
 	//Sets dropdown to current access level
 	$("#modSlapAccessLevel").val(_slapSettings.accessLevel).change();
+	
+	//Saves new accessLevel
 	$("#modSlapAccessLevel").change(function() {
 		_slapSettings.accessLevel = $("#modSlapAccessLevel").val();
+		_saveSettings();
+	});
+	
+	//Sets input field to current modSlapSettings.message value
+	$("#modSlapMessageInput").val(_slapSettings.message).change();
+	
+	//Updates modSlapSettings.message based on the value of the input field
+	$("#modSlapMessageInput").change(function() {
+		_slapSettings.message = $("#modSlapMessageInput").val();
 		_saveSettings();
 	});
 	
@@ -76,17 +101,17 @@ var modSlap = (function(){
 	var _slapCmd = function (params, from, mod, subscriber) {
 
 		if(_slapSettings.accessLevel == "mod" && !mod) {
-			apiLog(`AccessLevel mod and ${from} is not a mod`);
+			//apiLog(`AccessLevel mod and ${from} is not a mod`);
 			return;
 		}
 		
 		if(_slapSettings.accessLevel == "sub" && !subscriber) {
-			apiLog(`AccessLevel sub and ${from} is not a sub`);
+			//apiLog(`AccessLevel sub and ${from} is not a sub`);
 			return;
 		}
 		
 		if(_slapSettings.accessLevel == "modSub" && (!mod && !subscriber)) {
-			apiLog(`AccessLevel modSub and ${from} is not a mod or a sub`);
+			//apiLog(`AccessLevel modSub and ${from} is not a mod or a sub`);
 			return;
 		}
 		
@@ -99,9 +124,17 @@ var modSlap = (function(){
 		if ( target.toLowerCase() == from.toLowerCase() ) {
 			return apiSay(`${from} why would you want to slap yourself?`);
 		} else {
-			return apiSay(`${apiGetBotName()} lurches forth and slaps some sense into ${target}!`);
+			var message = _slapSettings.message;
+			//apiLog(`${message}`);
+			message = message.replace( /\[botname\]/g, apiGetBotName());
+			//apiLog(`${message}`);
+			message = message.replace( /\[user\]/g, from);
+			//apiLog(`${message}`);
+			message = message.replace( /\[target\]/g, target);
+			//apiLog(`${message}`);
+			return apiSay(message);
 		}
-	}
+	};
 	
 	return {
 		slapCmd : _slapCmd
